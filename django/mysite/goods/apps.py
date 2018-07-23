778fb9bdf8742f3460af6ca8830d9d2c042522e6
xiangjl <xiangjl@glodon.com>
2018-07-23 11:06:40 +0800

    提交Admin Demo 注意
    class GoodsConfig(AppConfig):
        name = 'goods'
        verbose_name = '我的商店'  #不生效

--- django/mysite/goods/apps.py
+++ django/mysite/goods/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class GoodsConfig(AppConfig):
     name = 'goods'
+    verbose_name = '我的商店'  #不生效
+
--- django/mysite/goods/models.py
+++ django/mysite/goods/models.py
@@ -2,6 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Goods(models.Model):
-    goods_name = models.CharField(max_length=30)
-    goods_number = models.IntegerField()
-    goods_price = models.FloatField()
+    goods_name = models.CharField('商品名称',max_length=30)
+    goods_number = models.IntegerField('数量')
+    goods_price = models.FloatField('价格')
+    goods_sales = models.IntegerField('销量', default=0)
+    class Meta:
+        verbose_name_plural = "商品管理"
+        verbose_name = "商品"
+    def __str__(self):
+       return self.goods_name
+
+    def sales_volume(self):
+        return self.goods_sales * self.goods_price
+
+    sales_volume.short_description = '销售额'
+    goods_amount = property(sales_volume)
