ffa472dd5f51ae5317006100e2d2899c39af5371
xiangjl <xiangjl@glodon.com>
2018-07-24 08:50:33 +0800

    创建 模型
    完成数据库的创建之后，就可以在项目文件夹下通过“makemigrations”和“migrate”命令进行数据表的创建

--- django/myblog/myblog/settings.py
+++ django/myblog/myblog/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
 TIME_ZONE = 'UTC'
 
@@ -112,7 +112,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
