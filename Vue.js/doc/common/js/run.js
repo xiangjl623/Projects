ed22235c90c88459813b4e061b6d7aae3614ba71
xiangjl <xiangjl@glodon.com>
2019-01-23 17:24:40 +0800

    框架更新

--- Vue.js/doc/common/js/run.js
+++ Vue.js/doc/common/js/run.js
@@ -1,20 +1,18 @@
-(function(){
-           
-    function run(code){
-        var newWin = window.open("", "_blank", "");
-        newWin.opener = null; // 防止代码对页面修改
-        newWin.document.open();
-        newWin.document.write(code);
-        newWin.document.close();
-    }
-     
-    //遍历页面中运行代码按钮
-    var  executes = document.getElementsByName("run");
-    for(var i=0; i<executes.length; i++){
-        executes[i].onclick = function(){
-            var code = this.form.codearea.value;
-            run(code);
-        };
-    }
-     
-}());
+function run(e){
+	var codeid = e.getAttribute("code-id");	
+	var resultid = e.getAttribute("result-id");	
+	var code = $("#"+codeid).val();
+	var iframe = document.getElementById(resultid);
+    var iwindow = iframe.contentWindow;
+    var idoc = iwindow.document;
+	idoc.open();
+	idoc.write(code);
+	idoc.close();
+}
+
+function binddata(id, file)
+{
+   $.get(file, function(data) {
+	 $("#" + id).text(data);
+   }, 'text');
+}
