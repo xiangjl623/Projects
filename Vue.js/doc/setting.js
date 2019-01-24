b63fc583b8bfb7131c9f4b00c10cb7d813ff1ff9
xiangjl <xiangjl@glodon.com>
2019-01-24 14:00:02 +0800

    Vue.js模板更新

--- Vue.js/doc/setting.js
+++ Vue.js/doc/setting.js
@@ -2,5 +2,9 @@ var zNodes =[
    { id:1,  pId:0,  name:"基础", file:""},
    { id:2,  pId:1,  name:"介绍", file:""},
    { id:3,  pId:2,  name:"声明式渲染", file:"01-基础/01-介绍/01声明式渲染.xml"},
-   { id:4,  pId:2,  name:"条件与循环", file:"01-基础/01-介绍/02条件与循环.xml"},
-   { id:5,  pId:0,  name:"Test02", file:""}]
+   { id:4,  pId:2,  name:"条件", file:"01-基础/01-介绍/02条件.xml"},
+   { id:5,  pId:2,  name:"条件与循环", file:"01-基础/01-介绍/02条件与循环.xml"},
+   { id:6,  pId:2,  name:"循环", file:"01-基础/01-介绍/03循环.xml"},
+   { id:7,  pId:2,  name:"处理用户输入", file:"01-基础/01-介绍/04处理用户输入.xml"},
+   { id:8,  pId:2,  name:"组件化应用构建", file:"01-基础/01-介绍/05组件化应用构建.xml"},
+   { id:9,  pId:0,  name:"Test02", file:""}]
