b415d6b4b1e2ddbb4235ff599b942d5f895cf89a
xiangjl <xiangjl@glodon.com>
2019-01-25 10:05:20 +0800

    Vue.js更新Demo

--- Vue.js/doc/common/js/run.js
+++ Vue.js/doc/common/js/run.js
@@ -3,12 +3,22 @@ function run(e){
 	var resultid = e.getAttribute("result-id");	
 	var code = $("#"+codeid).val();
 	code = "<script src='" + "common/js/vue.js'" + "></script>" + code;
+	code = "<script src='" + "common/js/utils.js'" + "></script>" + code;
 	var iframe = document.getElementById(resultid);
     var iwindow = iframe.contentWindow;
     var idoc = iwindow.document;
 	idoc.open();
 	idoc.write(code);
 	idoc.close();
+}
+
+function savecode(e){
+	var codeid = e.getAttribute("code-id");	
+	var fileid = e.getAttribute("file-id");
+	var code = $("#"+codeid).val();
+    var file = $("#"+fileid).val();
+    var blob = new Blob([code], {type: "xml;charset=utf-8"});
+    saveAs(blob, file);	
 }
 
 function binddata(id, file)
